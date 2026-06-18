52c32cda083fdc2f2a846eafc46b03aabe5db560
SakshamJain258 <jsaksham677@gmail.com>
2026-06-19 02:16:34 +0530

    Updated the Download link

--- lib/site-links.ts
+++ lib/site-links.ts
@@ -4,5 +4,5 @@ export const siteLinks = {
   github: 'https://github.com/SakshamJain258/Gestura_v2',
   linkedIn: 'https://www.linkedin.com/in/saksham-jain-6a74b128a/',
   researchPaper: 'https://drive.google.com/file/d/1zT-9kl8uMixt35fh_YmMm7v4dmrmR6jh/view?usp=sharing',
-  download: 'https://drive.google.com/file/d/1ul-qyx1YuGjQkf-dsgJaNwLff2gCzfZ2/view?usp=sharing',
+  download: 'https://d2pymib7m6uo9e.cloudfront.net/Gestura_v1.zip',
 } as const
